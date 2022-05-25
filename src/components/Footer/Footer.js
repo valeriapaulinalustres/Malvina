dc50b778f21c9092ae5af2ea9f056a67a45f9779
Gabriela Esther Mortensen <gabrielamortensen@gmail.com>
2022-05-24 21:35:55 -0300

    footer listo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,27 +6,30 @@ import './footer.css';
 function NavBar() {
     return (
         <nav className='footer'>
-            <div>
-                <NavLink to="/" className='seccionesFooter' >HOME</NavLink>
+            <div className='seccionesFooter'>
+                <NavLink to="/" className='seccionesFooterLink' >HOME</NavLink>
             </div>
-            <div>
-                <NavLink to="/shop" className='seccionesFooter'>SHOP</NavLink>
-                <NavLink to="/shop/categories/remeras" className='shopSeccion'>Remeras</NavLink>
-                <NavLink to="/shop/categories/camisas">Camisas</NavLink>
-                <NavLink to="/shop/categories/buzos">Buzos</NavLink>
-                <NavLink to="/shop/categories/sweaters">Sweaters</NavLink>
-                <NavLink to="/shop/categories/bermudas">Bermudas</NavLink>
-                <NavLink to="/shop/categories/pantalones">Pantalones</NavLink>
-                <NavLink to="/shop/categories/jeans">Jeans</NavLink>
-                <NavLink to="/shop/categories/camperas">Camperas</NavLink>
+            <div className='seccionesFooter'>
+                <NavLink to="/shop" className='seccionesFooterLink'>SHOP</NavLink>
+                <NavLink to="/shop/categories/remeras"  className='seccionShop'>Remeras</NavLink>
+                <NavLink to="/shop/categories/camisas" className='seccionShop'>Camisas</NavLink>
+                <NavLink to="/shop/categories/buzos" className='seccionShop'>Buzos</NavLink>
+                <NavLink to="/shop/categories/sweaters" className='seccionShop'>Sweaters</NavLink>
+                <NavLink to="/shop/categories/bermudas" className='seccionShop'>Bermudas</NavLink>
+                <NavLink to="/shop/categories/pantalones" className='seccionShop'>Pantalones</NavLink>
+                <NavLink to="/shop/categories/jeans" className='seccionShop'>Jeans</NavLink>
+                <NavLink to="/shop/categories/camperas" className='seccionShop'>Camperas</NavLink>
             </div>
-            <div>
-                <NavLink to="/about" className='seccionesFooter'>ABOUT US</NavLink>
-                
+            <div className='seccionesFooter'>
+                <NavLink to="/about" className='seccionesFooterLink'>ABOUT US</NavLink>
+            </div>    
+            <div className='seccionesFooter'>
+                <NavLink to="/contact" className='seccionesFooterLink'>CONTACT</NavLink>
+                <h2 className='seccionShop'>malvinaXXX@mail</h2>
             </div>
-            <div>
-                <NavLink to="/contact" className='seccionesFooter'>CONTACT</NavLink>
-                <h2 className='seccionesFooter'>malvinaXXX@mail</h2>
+
+            <div className='seccionesFooter'>
+                <NavLink to="/" className='malvina'> MALVINA </NavLink>
             </div>
         </nav>
     )
